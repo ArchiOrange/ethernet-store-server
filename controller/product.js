52a17a878178dda5e6f422c9ea6daeba1ddaa56b
unknown <ArchiOrange>
2020-09-13 18:04:45 +0700

    added function validation data product on the emptness and translite string to number

--- controller/product.js
+++ controller/product.js
@@ -1,12 +1,41 @@
 const Model  = require('../model/product')
+function validation(product) {
+  let errorEntryData  = []
+  for (var variable in product) {
+    if (product[variable] == '') {
+      errorEntryData.push(`${variable}: property is empty`)
+    }
+  }
+  for (var i = 0; i < product.specifications.length; i++) {
 
+    if (product.specifications[i].value == '') {
+      errorEntryData.push(`${product.specifications[i].name}: is empty`)
+    }
+  }
+  if(!errorEntryData.length){
+    product.price = Number(product.price)
+    let specifications = product.specifications.map(function (item) {
+        if(isNaN(Number(item.value))){
+          return(item.value)
+        }
+        else{
+          return(Number(item.value))
+        }
+      })
+      product.specifications = specifications
+      return(product)
+  }else{
+    console.error(Error("errorEntryData"),errorEntryData);
+  }
+}
 exports.addProduct = function (req, res) {
   let product = JSON.parse(req.body.product)
   let img =  req.files.map(function (item,i) {
       return item.filename
     })
     product.img = img
-    Model.addProduct(product, function (err) {
+
+    Model.addProduct(validation(product), function (err) {
       if (err) {
         res.json(err)
       }
